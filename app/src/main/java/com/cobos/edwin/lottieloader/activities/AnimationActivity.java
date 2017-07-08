7230e13f31cd36e53f769d1ade85363bcdf633a1
Edwin-Cobos <Edwin-Cobos>
2017-07-08 03:12:03 -0500

    Added Switch of Loop animation and Background change color

--- app/src/main/java/com/cobos/edwin/lottieloader/activities/AnimationActivity.java
+++ app/src/main/java/com/cobos/edwin/lottieloader/activities/AnimationActivity.java
@@ -4,17 +4,17 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
-import com.airbnb.lottie.Cancellable;
 import com.airbnb.lottie.ImageAssetDelegate;
 import com.airbnb.lottie.LottieAnimationView;
-import com.airbnb.lottie.LottieComposition;
 import com.airbnb.lottie.LottieImageAsset;
-import com.airbnb.lottie.OnCompositionLoadedListener;
 import com.cobos.edwin.lottieloader.R;
 
 import org.json.JSONException;
@@ -33,17 +33,27 @@ public class AnimationActivity extends AppCompatActivity {
     private String animationJson;
     private String animationAssetsFolder;
     private File animationFile;
+    private LinearLayout animationContainer;
+    private int[] colorOptions;
+    private int currentColorIndex;
+    private boolean isAnimationLoop;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_animation);
+
         loadArgs();
 
         getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle(animationFile.getName());
+
         setupViews();
+
+        createColorOptions();
+
+        setBackgroundColor();
     }
 
     private void loadArgs() {
@@ -53,7 +63,79 @@ public class AnimationActivity extends AppCompatActivity {
         animationAssetsFolder = String.format("%1s/%2s", animationFile.getParent(), "images");
     }
 
-    public String readFileAsString(File file) {
+    @Override
+    protected void onResume() {
+        super.onResume();
+        loadAnimation();
+    }
+
+    private void setupViews() {
+        lottieAnimationView = (LottieAnimationView) findViewById(R.id.animation_view);
+        animationContainer = (LinearLayout) findViewById(R.id.animation_container);
+    }
+
+    private void createColorOptions() {
+        currentColorIndex = 0;
+        colorOptions = new int[]{
+                R.color.BackgroundOne,
+                R.color.BackgroundTwo,
+                R.color.BackgroundThree};
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.animation_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            case R.id.loop_action:
+                switchLoop(item);
+                return true;
+            case R.id.background_action:
+                switchBackgroundColor();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void switchBackgroundColor() {
+        currentColorIndex = currentColorIndex + 1 >= colorOptions.length ? 0 : ++currentColorIndex;
+        setBackgroundColor();
+    }
+
+    private void setBackgroundColor() {
+        animationContainer.setBackgroundColor(ContextCompat.getColor(this, colorOptions[currentColorIndex]));
+    }
+
+    private void switchLoop(MenuItem item) {
+        isAnimationLoop = !isAnimationLoop;
+
+        cancelAnimation();
+        playAnimation();
+
+        String toastMeesage = isAnimationLoop ? "Loop: Enable" : "Loop: Disable";
+        Toast.makeText(this, toastMeesage, Toast.LENGTH_LONG).show();
+    }
+
+    private void playAnimation() {
+        lottieAnimationView.loop(isAnimationLoop);
+        lottieAnimationView.playAnimation();
+    }
+
+    private void cancelAnimation() {
+        if (lottieAnimationView.isAnimating()) {
+            lottieAnimationView.cancelAnimation();
+        }
+    }
+
+    private String readFileAsString(File file) {
         StringBuilder stringBuilder = new StringBuilder();
         String line;
         BufferedReader in = null;
@@ -72,36 +154,15 @@ public class AnimationActivity extends AppCompatActivity {
         return stringBuilder.toString();
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                onBackPressed();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        loadAnimation();
-    }
-
-    private void setupViews() {
-        lottieAnimationView = (LottieAnimationView) findViewById(R.id.animation_view);
-    }
-
     private void loadAnimation() {
         if (animationJson != null && !animationJson.isEmpty()) {
             try {
                 lottieAnimationView.setImageAssetDelegate(new ImageAssetDelegate() {
                     @Override
                     public Bitmap fetchBitmap(LottieImageAsset asset) {
-                        File imageFile = new File(animationAssetsFolder+"/"+asset.getFileName());
+                        File imageFile = new File(animationAssetsFolder + "/" + asset.getFileName());
 
-                        if(imageFile.exists()){
+                        if (imageFile.exists()) {
                             Bitmap bmp = BitmapFactory.decodeFile(imageFile.getAbsolutePath());
                             return bmp;
                         }
@@ -109,8 +170,7 @@ public class AnimationActivity extends AppCompatActivity {
                     }
                 });
                 lottieAnimationView.setAnimation(new JSONObject(animationJson));
-                lottieAnimationView.loop(false);
-                lottieAnimationView.playAnimation();
+                playAnimation();
             } catch (JSONException e) {
                 e.printStackTrace();
             }
